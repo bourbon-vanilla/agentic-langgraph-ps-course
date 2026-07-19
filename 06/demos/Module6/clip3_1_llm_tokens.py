4eaf83a552acab6ee94c010d8f672a35071a1309
Raphael <bourbon-vanilla@outlook.com>
2026-07-19 19:08:24 +0200

    chore: make chapter 6 working

--- 06/demos/Module6/clip3_1_llm_tokens.py
+++ 06/demos/Module6/clip3_1_llm_tokens.py
@@ -1,6 +1,6 @@
 # Chatbot with Streaming
 
-import warnings 
+import warnings, os
 warnings.filterwarnings("ignore", category=UserWarning, module="langchain_tavily")
 
 from typing import TypedDict, Annotated
@@ -32,7 +32,11 @@ def sub(a: int, b: int) -> int:
     """Return the difference of two numbers."""
     return a - b
 
-llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatOpenAI(
+    model=os.environ["CUSTOM_OPENAI_MODEL"],
+    base_url=os.environ["CUSTOM_OPENAI_ENDPOINT"],
+    api_key=os.environ["CUSTOM_OPENAI_API_KEY"],
+)
 
 tools = [tavily_tool, add, sub]
 llm_with_tools = llm.bind_tools(tools)
